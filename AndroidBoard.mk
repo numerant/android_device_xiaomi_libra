8428fa8585c73d172b4b06960ae45c9a7e77b0c4
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-02-12 22:59:33 +0200

    Update WiFi

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -11,8 +11,6 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # Create symbolic links
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
-        ln -sf  /system/etc/wifi/WCNSS_qcom_wlan_nv.bin \
-        $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_wlan_nv.bin; \
         ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
         $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
         ln -sf /persist/wlan_mac.bin \
