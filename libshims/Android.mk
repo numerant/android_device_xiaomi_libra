2f07975450d71643ec97c892640205d5e9bfb9aa
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-03-11 19:41:34 +0200

    Shim thermal-engine

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -24,3 +24,15 @@ LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := thermal_engine.c
+
+LOCAL_MODULE := libshim_thermalengine
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 64
+
+include $(BUILD_SHARED_LIBRARY)
